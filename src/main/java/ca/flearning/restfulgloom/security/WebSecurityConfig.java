c701d3ef50d68a178da0ba8f7b3e0e2af44eb324
Trequetrum <Mrk.C.Fischer@gmail.com>
2020-05-08 13:06:05 -0400

    Securend ALL rest Endpoints for Admin Role

--- src/main/java/ca/flearning/restfulgloom/security/WebSecurityConfig.java
+++ src/main/java/ca/flearning/restfulgloom/security/WebSecurityConfig.java
@@ -32,9 +32,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			// root, home, & registration open to public
-			.antMatchers("/", "/home", "/registration", "/api", "/api/**").permitAll()
+			.antMatchers("/", "/home", "/registration").permitAll()
 			//allow h2 console access to admins only
-			.antMatchers("/h2-console/**").hasRole("ADMIN")
+			.antMatchers("/h2-console/**", "/api**").hasRole("ADMIN")
 			.anyRequest().authenticated();
 		http.csrf()
 			// Don't apply CSRF protection to /h2-console. 
